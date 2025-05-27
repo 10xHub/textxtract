6685ec262911277c2ca2c1a2237856f40366c04b
Shudipto Trafder <shudiptotrafder@gmail.com>
2025-05-27 17:19:41 +0600

    v 0.2.2 published

--- textxtract/__init__.py
+++ textxtract/__init__.py
@@ -4,7 +4,7 @@ from textxtract.sync.extractor import SyncTextExtractor
 from textxtract.aio.extractor import AsyncTextExtractor
 from textxtract.core.config import ExtractorConfig
 
-__version__ = "0.2.0"
+__version__ = "0.2.2"
 __all__ = [
     "SyncTextExtractor",
     "AsyncTextExtractor",
